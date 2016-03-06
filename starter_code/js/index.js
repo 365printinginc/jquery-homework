11873507c81231af17f1ba854b3b46a78f66ae03
365printinginc <lisa.kim@365printinginc.com>
2016-03-05 21:10:03 -0800

    homework

--- starter_code/js/index.js
+++ starter_code/js/index.js
@@ -1,25 +1,70 @@
 //Implement the red light using jQuery. Don't forget to add the script tags.
+ $(document).ready(pageReady);
+    function pageReady() { 
+        $("#readless1").hide();
+        $("#section1 p").hide();   
+        $("#readless2").hide();
+        $("#section2 p").hide();
+        $("#sidebar2 p").hide();
+    }
 
-$("#stop-button").click(illuminatedRed);
-$("#slow-button").click(illuminatedYellow);
-$("#go-button").click(illuminatedGreen);
+  $("#readmore1").click(handleClick);
+    function handleClick() {
+        event.preventDefault();
+     	}
 
-function illuminatedRed() {
-	clearLights();
-	$("#stop-light").css("background-color", "red");
+  $("#readmore2").click(handleClick);
+    function handleClick() {
+        event.preventDefault();
+     	}
+
+  $("#readless1").click(handleClick);
+    function handleClick() {
+        event.preventDefault();
+     	}
+
+  $("#readless2").click(handleClick);
+    function handleClick() {
+        event.preventDefault();
+     	}
+
+  $("#learnmore").click(handleClick);
+    function handleClick() {
+        event.preventDefault();
+     	}
+
+
+$("#readmore1").click(readMore1SlideDown);
+$("#readmore2").click(readMore2SlideDown);
+$("#readless1").click(readLess1SlideUp);
+$("#readless2").click(readLess2SlideUp);
+$("#learnmore").click(learnMoreSlideDown);
+
+function readMore1SlideDown() {
+	$("#section1 p").slideDown("slow");
+	$("#readless1").show();
+	$("#readmore1").hide();
 }
 
-function illuminatedYellow() {
-	clearLights();
-	$("#slow-light").css("background-color", "yellow");
+function readMore2SlideDown() {
+	$("#section2 p").slideDown("slow");
+	$("#readless2").show();
+	$("#readmore2").hide();
 }
 
-function illuminatedGreen() {
-	clearLights();
-	$("#go-light").css("background-color", "green");
+function readLess1SlideUp() {
+	$("#section1 p").slideUp("slow");
+	$("#readless1").hide();
+	$("#readmore1").show();
 }
 
-function clearLights() {
-	$(".bulb").css("background-color", "black");
+function readLess2SlideUp() {
+	$("#section2 p").slideUp("slow");
+	$("#readless2").hide();
+	$("#readmore2").show();
 }
 
+function learnMoreSlideDown() {
+	$("#sidebar2 p").slideDown("slow");
+	$("#learnmore").hide();
+}
